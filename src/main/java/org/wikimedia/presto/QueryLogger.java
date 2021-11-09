8e1155350beb7f79f413abc25c70ae0b8f79b360
Razzi Abuissa <rabuissa@wikimedia.org>
2021-11-09 13:16:01 -0600

    Rename App to QueryLogger

---
+++ src/main/java/org/wikimedia/presto/QueryLogger.java
@@ -0,0 +1,11 @@
+package org.wikimedia.presto;
+
+import io.prestosql.spi.eventlistener.EventListener;
+
+/**
+ * Hello world!
+ *
+ */
+public class QueryLogger implements EventListener {
+    public QueryLogger() {}
+}
