4ea8c5210c3fa3db9eda65a326a968eba8cf3667
Razzi Abuissa <rabuissa@wikimedia.org>
2021-11-09 12:58:11 -0600

    Starting point with build copied from gobblin-wmf

---
+++ src/main/java/org/wikimedia/presto/App.java
@@ -0,0 +1,13 @@
+package org.wikimedia.presto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
