5b374415a698c3f1301019b78b33b9c1658213e0
Razzi Abuissa <rabuissa@wikimedia.org>
2021-11-09 13:41:31 -0600

    Starting template

--- src/main/java/org/wikimedia/presto/QueryLogger.java
+++ src/main/java/org/wikimedia/presto/QueryLogger.java
@@ -1,11 +1,24 @@
 package org.wikimedia.presto;
 
+import java.util.Map;
+
 import com.facebook.presto.spi.eventlistener.EventListener;
+import com.facebook.presto.spi.eventlistener.QueryCompletedEvent;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
- * Hello world!
+ * Send Presto queries to EventGate.
  *
  */
+@Slf4j
 public class QueryLogger implements EventListener {
-    public QueryLogger() {}
+    public QueryLogger(Map<String, String> config) {}
+
+    @Override
+    public void queryCompleted(QueryCompletedEvent queryCompletedEvent) {
+        EventListener.super.queryCompleted(queryCompletedEvent);
+        log.info("Query completed");
+        // @TODO extract information out of queryCompletedEvent
+    }
 }
--- src/test/java/org/wikimedia/presto/AppTest.java
+++ src/test/java/org/wikimedia/presto/AppTest.java
@@ -5,16 +5,14 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * TODO.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
-     * Rigorous Test :-)
+     * TODO.
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
